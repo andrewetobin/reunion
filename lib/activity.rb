e29d3c23b46311da2e14fa5be0ccdae4f4b6ca82
andrewetobin <andrewetobin@gmail.com>
2018-05-30 10:54:51 -0600

    initial commit

--- lib/activity.rb
+++ lib/activity.rb
@@ -1,2 +1,20 @@
 class Activity
+  attr_reader :activity_name,
+              :participants,
+              :participant_name,
+              :base_cost
+
+  def initialize(name, base_cost = 0)
+    @activity_name = name
+    @participants = []
+    @base_cost = base_cost
+  end
+
+  def add_participants(participants)
+    @participants = participants
+  end
+
+  def participant_cost
+    base_cost.to_f / participants.length
+  end
 end
--- test/activity_test.rb
+++ test/activity_test.rb
@@ -4,4 +4,50 @@ require 'minitest/pride'
 require './lib/activity'
 
 class ActivityTest < Minitest::Test
+  def test_it_exists
+    activity_1 = "kickball"
+    activity = Activity.new(activity_1)
+
+    assert_instance_of Activity, activity
+  end
+
+  def test_it_has_a_name
+    activity_1 = "kickball"
+    activity = Activity.new(activity_1)
+
+    assert_equal "kickball", activity.activity_name
+  end
+
+  def test_activity_starts_with_no_participants
+    activity_1 = "kickball"
+    activity = Activity.new(activity_1)
+
+    assert_equal [], activity.participants
+
+  end
+
+  def test_it_has_a_participant
+    participant_1 = {"Heather"=> 5}
+    activity_1 = "kickball"
+    activity = Activity.new(activity_1)
+
+    assert_equal 1, activity.add_participants(participant_1).length
+  end
+
+  def test_activity_has_a_base_cost
+    participant_1 = {"Heather"=> 5}
+    activity_1 = "kickball"
+    activity = Activity.new(activity_1, 300)
+
+    assert_equal 300, activity.base_cost
+  end
+
+  def test_it_has_price_per_person
+    participants = {"Heather"=> 5}, {"Felix"=> 43}, {"Bob"=> 23}
+    activity_1 = "kickball"
+    activity = Activity.new(activity_1, 300)
+    activity.add_participants(participants)
+
+    assert_equal 100, activity.participant_cost
+  end
 end
